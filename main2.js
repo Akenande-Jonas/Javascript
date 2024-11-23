3c1669e2a12da4e48520ca79b3a40450681aaef3
A. Jonas Jr <jonasakenande7@gmail.com>
2024-11-23 20:53:53 +0100

    commit2

---
+++ main2.js
@@ -0,0 +1,6 @@
+var MavariableString = "Bonsoir les ami(e)(s)";
+var MavariableNumerique = 100;
+var MavariableTableau = ['Apple', 'Banana'];
+var MavariableObjet = window;
+
+alert(MavariableString + MavariableNumerique + MavariableTableau[0] + MavariableObjet.location.pathname);

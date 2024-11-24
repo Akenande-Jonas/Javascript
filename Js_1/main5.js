7fc7cd654639ab61ef6c6c3d65a83fbcfa4df3a4
A. Jonas Jr <jonasakenande7@gmail.com>
2024-11-24 20:22:00 +0100

    éh

---
+++ Js_1/main5.js
@@ -0,0 +1,12 @@
+// chaine de caractere
+var MavariableString = "fenetre 300px";
+// nombre
+var MavariableNumerique = prompt ("entrez un mot", 100);
+// objet
+var MavariableObjet = window
+// condition 
+if (MavariableString =="fenetre 300px" && MavariableNumerique !=400 || MavariableNumerique >= 300){
+    alert("C'est inferieur à 300px");
+} else {
+    alert("C'est toujours sup à 300px");
+}

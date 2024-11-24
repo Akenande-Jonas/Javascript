34774e520a4c18db22575a1747147cac69a8064a
A. Jonas Jr <jonasakenande7@gmail.com>
2024-11-24 17:01:59 +0100

    v1.1

--- main2.js
+++ main2.js
@@ -1,6 +1,13 @@
+//chaine de caractere
 var MavariableString = "Bonsoir les ami(e)(s)";
+//nombre
 var MavariableNumerique = 100;
+//tableau
 var MavariableTableau = ['Apple', 'Banana'];
-var MavariableObjet = window;
+// objet
+var MavariableObjet = window
+    var MavariableSurveillance = window
+        var window = innerHeight = 911;
+        var window = innerWidth = 811;   
 
-alert(MavariableString + MavariableNumerique + MavariableTableau[0] + MavariableObjet.location.pathname);
+alert(MavariableString + MavariableNumerique + MavariableTableau[0] + MavariableObjet.location.pathname + MavariableSurveillance);

2d48566e4ffaadac2aac7d1a08609b3a15b5ba4e
Jonas <site1@gmail.com>
2024-11-26 15:34:26 +0100

    1

--- test/main.js
+++ test/main.js
@@ -1,1 +1,6 @@
-alert("salut");
+let iceCream = "chocolat";
+if (iceCream === "chocolat") {
+  alert("J'adore la glace au chocolat !");
+} else {
+  alert("Ooooh, mais j'aurais préféré au chocolat.");
+}

8855d115fdff6fab863fd10b1773783a74cfcc07
A. Jonas Jr <jonasakenande7@gmail.com>
2024-11-28 00:39:12 +0100

    3

--- Js_1/main5.js
+++ Js_1/main5.js
@@ -1,12 +1,7 @@
-// chaine de caractere
-var MavariableString = "fenetre 300px";
-// nombre
-var MavariableNumerique = prompt ("entrez un mot", 100);
-// objet
-var MavariableObjet = window
-// condition 
-if (MavariableString =="fenetre 300px" && MavariableNumerique !=400 || MavariableNumerique >= 300){
-    alert("C'est inferieur à 300px");
-} else {
-    alert("C'est superieur à 300px");
+// variable (Caractère)
+var MaVariable = window.innerWidth
+// Condition
+if (MaVariable < 300){ 
+    alert("La largeur est à 300px !");
+
 }
